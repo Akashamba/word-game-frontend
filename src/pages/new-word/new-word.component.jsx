43546af98935a33c268b505bb0a1d73fc5c7a84a
Akashamba <akashambashankar@gmail.com>
2021-03-20 13:24:29 +0530

    fetch for every 10 words

--- src/pages/new-word/new-word.component.jsx
+++ src/pages/new-word/new-word.component.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Card from '../../components/word-card/word-card.component';
 
-function nextWord(flip, current, setCurrent) {
+function nextWord(flip, current, setCurrent, setWords) {
     flip(false)
     if(current<9) {
         setCurrent(current+1)
     }
-    else setCurrent(0)
+    else {
+        setCurrent(0)
+        fetchWords(setWords)
+    }
+}
+
+function fetchWords(setWords) {
+    fetch("https://sh-word-game-backend.herokuapp.com/")
+        .then(response => response.json())
+        .then(words => setWords(words.words))
 }
 
 export default function NewWord() {
@@ -14,16 +23,14 @@ export default function NewWord() {
     const [current ,setCurrent] = React.useState(0)
     const [isFlipped, flip] = React.useState(false)
     React.useEffect(() => {
-        fetch("https://sh-word-game-backend.herokuapp.com/")
-        .then(response => response.json())
-        .then(words => setWords(words.words))
-    })
+        fetchWords(setWords)
+    }, [])
 
     if(words) {
         return (
             <div className="new-word">
                 <Card word={words[current]} isFlipped={isFlipped} flip={flip} />
-                <button onClick={() => nextWord(flip, current, setCurrent)}>Click</button>
+                <button onClick={() => nextWord(flip, current, setCurrent, setWords)}>Click</button>
             </div>
         )
     }
