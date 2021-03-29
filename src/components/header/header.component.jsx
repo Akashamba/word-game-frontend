c8eba3452e6d5089544b437613cbea5c9b5a8934
Akashamba <akashambashankar@gmail.com>
2021-03-29 08:03:41 +0530

    removed sign in when signed in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -24,14 +24,18 @@ export default function Header({user, setUser}) {
                 <p className="sub-title">Build your Vocabulary</p>
                 <nav className="navbar">
                     <Link className="nav-link" onClick={() => setDrawOpen(false)} to="/">Learn</Link>
-                    <Link className="nav-link" onClick={() => setDrawOpen(false)} to="/sign-in">Sign In</Link>
-                    <Link className="nav-link" onClick={() => setDrawOpen(false)} to="/sign-up">Sign Up</Link>
+                    {!username?
+                        <div style={{display: "inline"}}>
+                            <Link className="nav-link" onClick={() => setDrawOpen(false)} to="/sign-in">Sign In</Link>
+                            <Link className="nav-link" onClick={() => setDrawOpen(false)} to="/sign-up">Sign Up</Link>
+                        </div>:null}
                     <Link className="nav-link" onClick={() => setDrawOpen(false)} to="/dashboard">Dashboard</Link>
                     {username?
                         <Link className="nav-link" style={{marginLeft: "10px"}} onClick={handleSignOut}>Sign Out from 
                         <span style={{fontWeight: "bold"}}> {username}</span></Link>
                         :null}
                 </nav>
+                
                 <MenuIcon id="menu-icon" onClick={() => setDrawOpen(true)}/>
                 <Drawer className="draw" anchor="right" open={drawOpen} onClose={() => setDrawOpen(false)}>
                     <CloseIcon id="close-icon" onClick={() => setDrawOpen(false)}/>
@@ -40,8 +44,11 @@ export default function Header({user, setUser}) {
                         <span style={{fontWeight: "bold"}}> {username}</span></Link>
                         :null}
                     <Link className="draw-nav-link" onClick={() => setDrawOpen(false)} to="/">Learn</Link>
-                    <Link className="draw-nav-link" onClick={() => setDrawOpen(false)} to="/sign-in">Sign In</Link>
-                    <Link className="draw-nav-link" onClick={() => setDrawOpen(false)} to="/sign-up">Sign Up</Link>
+                    {!username?
+                        <React.Fragment>
+                            <Link className="draw-nav-link" onClick={() => setDrawOpen(false)} to="/sign-in">Sign In</Link>
+                            <Link className="draw-nav-link" onClick={() => setDrawOpen(false)} to="/sign-up">Sign Up</Link>
+                        </React.Fragment>:null}
                     <Link className="draw-nav-link" onClick={() => setDrawOpen(false)} to="/dashboard">Dashboard</Link>
                 </Drawer>
             </Toolbar>
